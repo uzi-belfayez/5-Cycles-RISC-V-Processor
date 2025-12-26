894b2d866ebb6be7cc3c8647a89c6741dec40942
Rayen Belfayez <119884263+uzi-belfayez@users.noreply.github.com>
2025-12-26 12:30:33 +0100

    Fix formatting of ALU entity declaration

--- alu.vhd
+++ alu.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 use IEEE.numeric_std.ALL;
 
-entity alu is
+entity alu is 
     generic (
         dataWidth       : integer:=32;
         aluOpWidth      :   integer:=4
